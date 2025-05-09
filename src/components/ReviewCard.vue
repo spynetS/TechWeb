fb6fb2e0395d8b9faa57abc6ce6200b7d9025123
Alfred Roos <alfred@stensatter.se>
2025-05-10 01:36:07 +0200

    added review

---
+++ src/components/ReviewCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+ defineProps({
+     description: String,
+     name: String,
+     title: String
+ });
+</script>
+
+<template>
+    <div class="review-card card flex flex-col py-8 px-4 items-center ">
+        <p class="text-gray-300" >{{description}}</p>
+        <p class="mt-3 font-semibold text-lg">{{name}}</p>
+        <p class="text-gray-300 text-xs">{{title}}</p>
+
+        <div class="mt-3 flex flex-row items-between">
+            <img src="@/assets/star.svg" alt="star" />
+            <img src="@/assets/star.svg" alt="star" />
+            <img src="@/assets/star.svg" alt="star" />
+            <img src="@/assets/star.svg" alt="star" />
+            <img src="@/assets/star.svg" alt="star" />
+
+        </div>
+    </div>
+</template>
