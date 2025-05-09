3fdfa97944147e90839ec727fb9768d36221efeb
Alfred <alfred@stensatter.se>
2025-05-09 10:16:58 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+
+
+</style>
